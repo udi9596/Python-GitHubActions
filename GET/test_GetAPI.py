f90a4024c1eadcff0632ddbeb82eb0692d2620f1
uday <udi9596@gmail.com>
2021-12-09 15:56:17 -0800

    Added GET API Pytest

--- GET/test_GetAPI.py
+++ GET/test_GetAPI.py
@@ -5,3 +5,9 @@ def test_GET():
 
     resp = requests.get(url)
     assert resp.status_code == 200
+
+def test_GET2():
+    url = "https://reqres.in/api/users/2"
+
+    resp = requests.get(url)
+    assert resp.status_code == 200
