22ab55679b9ef29f5afdf9043c4626a7afb4f231
uday <udi9596@gmail.com>
2021-12-09 16:02:34 -0800

    Added Print Statements for GET command tests

--- GET/test_GetAPI.py
+++ GET/test_GetAPI.py
@@ -4,10 +4,12 @@ def test_GET():
     url = "https://reqres.in/api/users?page=2"
 
     resp = requests.get(url)
+    print("TEST 1 Result : {}".format(resp.status_code))
     assert resp.status_code == 200
 
 def test_GET2():
     url = "https://reqres.in/api/users/2"
 
     resp = requests.get(url)
+    print("TEST 2 Result : {}".format(resp.status_code))
     assert resp.status_code == 200
